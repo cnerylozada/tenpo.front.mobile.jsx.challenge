bdbd6440d3938858206d86948497fb3dbcd3327d
cnerylozada <cnery@dynamicdevs.io>
2021-06-25 13:13:11 -0500

    feat: palette and fonts styles were created

--- pages/home.jsx
+++ pages/home.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { palette } from "../styles";
 
 const HomeView = () => {
   return (
     <View style={styles.container}>
-      <Text>Home view !</Text>
       <Text>Home view !</Text>
     </View>
   );
@@ -13,7 +13,7 @@ const HomeView = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "yellow",
+    backgroundColor: palette.white,
   },
 });
 
---
+++ styles/palette.js
@@ -0,0 +1,7 @@
+export const palette = {
+  black: "#223034",
+  white: "#ffffff",
+  gray: "#cecece",
+  green: "#218c74",
+  cyan: "#82ccdd",
+};
